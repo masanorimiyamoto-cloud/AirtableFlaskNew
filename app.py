4b3a52a19ae83de1ef954295ce4ebd90f72b99e5
masanorimiyamoto-cloud <mikekoro777@gmail.com>
2025-02-12 00:59:33 +0900

    memory

--- app.py
+++ app.py
@@ -296,5 +296,5 @@ def index():
                            workday=workday)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
